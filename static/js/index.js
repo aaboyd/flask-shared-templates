0aa6d9816a6326bcf8b04c744dc5dd5f1948fc3e
Alex Boyd <alex.boyd@outlook.com>
2014-02-09 01:54:58 -0500

    intial commit with simple example

---
+++ static/js/index.js
@@ -0,0 +1,13 @@
+﻿nunjucks.configure('/static/templates');
+
+$(function () {
+    $('button[type="submit"]').click(function (evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+
+        $('ul.list-group').append(nunjucks.render('_row.html', {
+            'title': $('#name').val(),
+            'body': $('#description').val()
+        }))
+    });
+});
